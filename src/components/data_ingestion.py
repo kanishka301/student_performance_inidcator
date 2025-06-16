c593909e2d397e3a52b1eef51a367c777a46bef7
kanishka301 <203.kanishka@gmail.com>
2025-06-16 14:31:24 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,21 +1,20 @@
-from src.exception import CustomException
+from dataclasses import dataclass
 from src.logger import logging
+from src.exception import CustomException
 import os
 import sys
 import pandas as pd
-from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-
-
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
 
 
 class DataIngestion():
@@ -31,23 +30,23 @@ class DataIngestion():
         returns: Tuple of three paths (train, test, and raw data)
         '''
 
-        logging.info('Initiating the data ingestion method or component')
+        logging.info('Initiating the data ingestion')
 
         try:
             # Read data from the source (database, local files, etc)
-            df = pd.read_csv('notebook/data/StdsPerform.csv')
+            data = pd.read_csv('notebook/data/StdsPerform.csv')
             logging.info('Data is read as a dataframe')
 
             # Creating directories
             logging.info('Create artifacts directory and save the raw data')
             os.makedirs(os.path.dirname(
                 self.ingestion_config.raw_data_path), exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,
+            data.to_csv(self.ingestion_config.raw_data_path,
                         index=False, header=True)
 
             # Split the data into train and test
             logging.info('Train test split initiated')
-            train, test = train_test_split(df, test_size=.2, random_state=42)
+            train, test = train_test_split(data, test_size=.2, random_state=42)
 
             logging.info('Saving train test split inside artifacts directory')
             train.to_csv(self.ingestion_config.train_data_path,
@@ -55,7 +54,7 @@ class DataIngestion():
             test.to_csv(self.ingestion_config.test_data_path,
                         index=False, header=True)
 
-            logging.info('Ingestion of the data is complete')
+            logging.info('Ingestion is complete')
 
             return (
                 self.ingestion_config.raw_data_path,
@@ -73,3 +72,5 @@ if __name__ == "__main__":
     dt = DataTransformation()
     train, test, preprocessor_path = dt.initiate_data_transformation(
         di.train_data_path, di.test_data_path)
+    mt = ModelTrainer()
+    print(mt.initiate_model_training(train, test))
